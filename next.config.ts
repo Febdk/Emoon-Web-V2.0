4f04ee5953ce9e8257645f4fc9116e7d15339820
Febdk <febydiky@gmail.com>
2026-06-10 12:49:27 +0700

    fix: set images unoptimized for netlify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Tambahkan baris ini untuk mengatasi error 502 di Netlify:
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
